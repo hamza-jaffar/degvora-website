44f0ad03906d6cb0d7eb90fa0bf0e83a577ef0f9
hamza-jaffar <hamzajaffer231006@gmail.com>
2026-02-07 19:09:07 +0500

    Add Banner

--- src/components/app-logo.tsx
+++ src/components/app-logo.tsx
@@ -2,7 +2,7 @@ const AppLogo = ({ showText = false }: { showText: boolean }) => {
   return (
     <div className="flex items-center gap-1">
       <img className="w-10 h-10" src="/assets/logo.svg" />
-      {showText && <p className="font-medium text-2xl">Degvora</p>}
+      {showText && <p className="font-medium  text-2xl">Degvora</p>}
     </div>
   );
 };
--- src/components/nav-bar.tsx
+++ src/components/nav-bar.tsx
@@ -2,10 +2,10 @@ import AppLogo from "./app-logo";
 
 const NavBar = () => {
   return (
-    <div className="w-full p-4 rounded-md shadow-lg flex justify-between items-center">
+    <div className="w-full p-4 max-w-7xl mx-auto fixed top-4 left-0 right-0 z-10 border-gray-100 border rounded-xl shadow-lg flex justify-between items-center">
       <AppLogo showText={true} />
 
-      <ul className="flex gap-5">
+      <ul className="gap-7 hidden md:flex ">
         <li className="cursor-pointer text-gray-700">Home</li>
         <li className="cursor-pointer text-gray-700">About</li>
         <li className="cursor-pointer text-gray-700">Services</li>
