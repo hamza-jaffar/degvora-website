0eba4be8322a38adad1558b4e2632685411e6b8c
hamza-jaffar <hamzajaffer231006@gmail.com>
2026-02-08 18:19:48 +0500

    Registation disable

--- resources/views/pages/auth/register.blade.php
+++ resources/views/pages/auth/register.blade.php
@@ -1,11 +1,11 @@
 <x-layouts::auth>
-    <div class="flex flex-col gap-6">
-        <x-auth-header :title="__('Create an account')" :description="__('Enter your details below to create your account')" />
+    <div class="flex text-center flex-col gap-6">
+        {{-- <x-auth-header :title="__('Create an account')" :description="__('Enter your details below to create your account')" /> --}}
 
         <!-- Session Status -->
-        <x-auth-session-status class="text-center" :status="session('status')" />
+        {{-- <x-auth-session-status class="text-center" :status="session('status')" /> --}}
 
-        <form method="POST" action="{{ route('register.store') }}" class="flex flex-col gap-6">
+        {{-- <form method="POST" action="{{ route('register.store') }}" class="flex flex-col gap-6">
             @csrf
             <!-- Name -->
             <flux:input
@@ -57,11 +57,21 @@
                     {{ __('Create account') }}
                 </flux:button>
             </div>
-        </form>
+        </form> --}}
 
-        <div class="space-x-1 rtl:space-x-reverse text-center text-sm text-zinc-600 dark:text-zinc-400">
-            <span>{{ __('Already have an account?') }}</span>
-            <flux:link :href="route('login')" wire:navigate>{{ __('Log in') }}</flux:link>
+        <h1 class="text-xl font-semibold">
+            Registration Disabled
+        </h1>
+
+        <p class="text-sm text-zinc-600 dark:text-zinc-400">
+            Creating a new account is currently not available.
+            If you already have an account, you can log in below.
+        </p>
+
+        <div class="text-sm">
+            <flux:link :href="route('login')" wire:navigate class="font-medium">
+                {{ __('Log in to your account') }}
+            </flux:link>
         </div>
     </div>
 </x-layouts::auth>
